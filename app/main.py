6f1f401fca5be8cb4719b433f6954c75c5d7b1b7
root <root@LAPTOP-RQGAA2VD.localdomain>
2025-07-29 21:09:47 +0200

    commit final

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ from app.predict import predict_churn
 # Utilisation de la bibliothèque FastApi pour créer l'application pour le modèle
 app = FastAPI()
 
-# Création de la route predict et de la fonction predict_churn
+# Création de la route predict et de la fonction client
 @app.post("/predict")
 def client(client: ClientData):
+    df = client.dict()
     df = predict_churn(df)
     return df
